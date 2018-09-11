a361898b2e85ab4e41eea102243949ddb8502fd3
Aritra-Aritra <aritra119.26@gmail.com>
2018-09-11 16:02:17 +0530

    Add unit tests

---
+++ api/utils/index.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+  return a + b;
+}
+
+// divide
+function divide(a, b) {
+  return a / b;
+}
+
+// multiple
+function multiply(a, b) {
+  return a * b;
+}
+
+// subtract
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  add: add,
+  divide: divide,
+  multiply: multiply,
+  subtract: subtract
+};
